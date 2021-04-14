9d7177ccbbb3fc445a1e88d6945ccd158ead8b6f
romes <rodrigo.m.mesquita@gmail.com>
2021-04-14 14:49:41 +0100

    Parser with Parsec by @btoninho

---
+++ STLC/parser/Lexer.hs
@@ -0,0 +1,39 @@
+
+module Lexer where
+
+
+import Text.Parsec
+import Text.Parsec.String
+import qualified Text.Parsec.Token as Tok
+import Text.Parsec.Language
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where ops = ["->","\\","+","*","-","=","()"]
+        names = ["True", "False","if","then","else","Z","succ","isZero","fst","snd","()"]
+        style = haskellStyle {Tok.reservedOpNames = ops,
+                              Tok.reservedNames = names,
+                              Tok.commentLine = "#"}
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+contents :: Parser a -> Parser a
+contents p = do
+  Tok.whiteSpace lexer
+  r <- p
+  eof
+  return r
+
+
+natural :: Parser Integer
+natural = Tok.natural lexer
---
+++ STLC/parser/Main.hs
@@ -0,0 +1,21 @@
+import Parser2
+
+
+import Control.Monad.Trans 
+import System.Console.Haskeline 
+
+process :: String -> IO ()
+process line =
+  let res = parseExpr line in
+  case res of
+    Left err -> print err
+    Right ex -> print ex
+
+main :: IO ()
+main = runInputT defaultSettings loop 
+    where 
+    loop = do 
+        minput <- getInputLine "> "
+        case minput of 
+            Nothing -> outputStrLn "Bye."
+            Just input -> (liftIO $ process input) >> loop
