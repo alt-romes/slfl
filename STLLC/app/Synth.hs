e4e763a1f5b7606a66f34807ace0a60cbcfc4ca8
romes <rodrigo.m.mesquita@gmail.com>
2021-05-16 12:11:31 +0100

    add delta_out to all synth functionsg

--- STLLC/app/Synth.hs
+++ STLLC/app/Synth.hs
@@ -9,19 +9,22 @@ import Syntax
 type Gamma = [(String, Type)] -- Unrestricted hypothesis
 type Delta = [(String, Type)] -- Linear hypothesis (not left asynchronous)
 type Omega = [(String, Type)] -- Ordered (linear?) hypothesis
-type Ctxt = (Gamma, Delta, Omega)
+type Ctxt = (Gamma, Delta, Omega) -- Delta out is a return value
 
 type FocusCtxt = (Gamma, Delta) -- Gamma, DeltaIn and DeltaOut in the return value
 
-synth :: Ctxt -> Type -> Expr
+synth :: Ctxt -> Type -> (Expr, Delta)
 
 ---- Right asynchronous rules -----------------
 
 ---- -oR
-synth (г, d, o) (Fun a b) = Abs "x" a $ synth (г, d, ("x", a):o) b
+synth (г, d, o) (Fun a b) = let (exp, d') = synth (г, d, ("x", a):o) b in
+                                 (Abs "x" a exp , d')
 
 ---- &R
-synth c (With a b) = WithValue (synth c a) (synth c b)
+synth c (With a b) = let (expa, d') = synth c a in
+                         let (expb, _) = synth c a in -- ?? DeltaOut from synth a and DeltaOut from synth b should be the same ??
+                             (WithValue expa expb, d')
 
 -- no more synchronous right propositions, start inverting the ordered context (omega)
 
@@ -29,13 +32,18 @@ synth c (With a b) = WithValue (synth c a) (synth c b)
 ---- Left asynchronous rules ------------------
 
 ---- *L
-synth (g, d, (n, Tensor a b):o) t = LetTensor "x" "y" (Var n) $ synth (g, d, ("y", b):("x", a):o) t
+synth (g, d, (n, Tensor a b):o) t = let (expt, d') = synth (g, d, ("y", b):("x", a):o) t in
+                                        (LetTensor "x" "y" (Var n) expt, d')
 
 ---- 1L
-synth (g, d, (n, Unit):o) t = LetUnit (Var n) $ synth (g, d, o) t
+synth (g, d, (n, Unit):o) t = let (expt, d') = synth (g, d, o) t in
+                                  (LetUnit (Var n) expt, d')
 
 ---- +L
-synth (g, d, (n, Plus a b):o) t = CaseOfPlus (Var n) "l" (synth (g, d, ("l", a):o) t) "r" (synth (g, d, ("r", b):o) t)
+synth (g, d, (n, Plus a b):o) t = let (expa, d') = synth (g, d, ("l", a):o) t in
+                                      let (expb, _) = synth (g, d, ("r", b):o) t in -- ?? como é que garanto que ambos estes synth vão usar os mesmos recursos?
+                                                                                    -- ? tenho de ter LogicT até nas invertíveis e andar para trás quando estas coisas correm mal? parece-me estranho,,
+                                          (CaseOfPlus (Var n) "l" expa "r" expb, d')
 
 ---- !L
 synth (g, d, (n, Bang a):o) t = synth ((n, a):g, d, o) t
@@ -56,28 +64,22 @@ synth (g, d, []) t = -- no more asynchronous propositions, focus
        observe $ focus (g, d) t
 
 
-focus :: FocusCtxt -> Type -> Logic Expr
-focus c t =  decideRight c t     -- because of laziness it'll only run until the first succeeds (bc of observe), right?
-         <|> decideLeft c t
-         <|> decideLeftBang c t    
+focus :: FocusCtxt -> Type -> Logic (Expr, Delta)
+focus c goal =  decideRight c goal     -- because of laziness it'll only run until the first succeeds (bc of observe), right?
+            <|> decideLeft c goal
+            <|> decideLeftBang c goal
     where
-        decideRight c t = do
-            (e, _) <- focus' Nothing c t
-            pure e
+        decideRight c goal = focus' Nothing c goal
 
-        decideLeft (g, din) t = do
+        decideLeft (g, din) goal = do
             case din of
               []     -> empty
-              a:din' -> do
-                  (e, _) <- focus' (Just a) (g, din') t
-                  pure e
+              a:din' -> focus' (Just a) (g, din') goal
 
-        decideLeftBang (g, din) t = do
+        decideLeftBang (g, din) goal = do
             case g of
               []   -> empty
-              a:g' -> do
-                  (e, _) <- focus' (Just a) (g, din) t
-                  pure e
+              a:g' -> focus' (Just a) (g, din) goal
         
         focus' :: Maybe (String, Type) -> FocusCtxt -> Type -> Logic (Expr, Delta)
 
@@ -85,14 +87,14 @@ focus c t =  decideRight c t     -- because of laziness it'll only run until the
 
         ---- *R
         focus' Nothing c@(g, d) (Tensor a b) = do
-            (expa, d') <- focus' Nothing c a
+            (expa, d') <- focus' Nothing c a  -- important todo: estou a assumir que aqui um possivel empty na chamada se propague e "termine a monad" ali, espero não estar a dizer nada demasiado ao lado. Eu tentei perceber o combinador com o que o prof enviou mas não consegui muito bemm
             (expb, d'') <- focus' Nothing (g, d') a
-            pure (TensorValue expa expb, d'')
+            return (TensorValue expa expb, d'')
 
         ---- 1R
         focus' Nothing c@(g, d) Unit = do
             guard (null d)
-            pure (UnitValue, d)
+            return (UnitValue, d)
             
         ---- +R
         focus' Nothing c (Plus a b) = do
@@ -100,12 +102,13 @@ focus c t =  decideRight c t     -- because of laziness it'll only run until the
             pure (InjL b il, d')
             <|> do
             (ir, d') <- focus' Nothing c b
-            pure (InjR a ir, d')
+            return (InjR a ir, d')
 
         ---- !R
         focus' Nothing c@(g, d) (Bang a) = do
             guard (null d)
-            pure (BangValue $ synth (g, d, []) a, d)
+            let (expa, d') = synth (g, d, []) a
+            return (BangValue expa, d')
 
         -- all right propositions focused on are synchronous; this pattern matching is extensive
 
@@ -115,9 +118,13 @@ focus c t =  decideRight c t     -- because of laziness it'll only run until the
 
         ---- Left synchronous rules -------------------
 
-        -- focus' (Just a) c t = 
+        focus' (Just (n, Fun a b)) c@(g, d) goal = do
+            (expb, d') <- focus' (Just ("g", b)) c goal
+            let (expa, d'') = synth (g, d', []) a
+            return (App expb expa, d'')
+            
 
 
 ---- top level
 
-synthType t = synth ([], [], []) t
+synthType t = fst $ synth ([], [], []) t
