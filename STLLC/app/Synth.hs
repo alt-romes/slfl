308996b80245139d0a56a413929fd6c8353eb6df
romes <rodrigo.m.mesquita@gmail.com>
2021-05-17 15:09:29 +0100

    fix last known bugs

--- STLLC/app/Synth.hs
+++ STLLC/app/Synth.hs
@@ -13,9 +13,9 @@ import Syntax
 type Gamma = [(String, Type)] -- Unrestricted hypothesis
 type Delta = [(String, Type)] -- Linear hypothesis (not left asynchronous)
 type Omega = [(String, Type)] -- Ordered (linear?) hypothesis
-type Ctxt = (Gamma, Delta, Omega) -- Delta out is a return value
+type Ctxt = (Gamma, Delta, Omega)   -- Delta out is a return value
 
-type FocusCtxt = (Gamma, Delta) -- Gamma, DeltaIn
+type FocusCtxt = (Gamma, Delta)     -- Gamma, DeltaIn
 
 type SynthState = Int  -- variable number to be used, note: should we also use the state monad for the delta context???
 
@@ -25,6 +25,12 @@ variableNames = ["x", "y", "z", "u", "v", "a", "b", "c", "d", "e", "f", "g", "h"
 
 getName :: Int -> String
 getName i = variableNames !! i
+
+isAtomic :: Type -> Bool
+isAtomic t = case t of
+               Bool -> True
+               Unit -> True
+               _ -> False
 
 synth :: Ctxt -> Type -> State SynthState (Expr, Delta)
 
@@ -113,7 +119,7 @@ focus c goal =
         decideLeft (g, din) goal = do
             case din of
               []     -> empty
-              a:din' -> focus' (Just a) (g, din') goal
+              a:din' -> trace ("DLeft + " ++ show a) $ focus' (Just a) (g, din') goal
 
         decideLeftBang (g, din) goal = do
             case g of
@@ -137,10 +143,10 @@ focus c goal =
             
         ---- +R
         focus' Nothing c (Plus a b) = do
-            (il, d') <- focus' Nothing c a
+            (il, d') <- trace "try injl" $ focus' Nothing c a
             return (InjL b il, d')
             <|> do
-            (ir, d') <- focus' Nothing c b
+            (ir, d') <- trace "try injr" $ focus' Nothing c b
             return (InjR a ir, d')
 
         ---- !R
@@ -151,7 +157,7 @@ focus c goal =
             lift $ put vari'
             return (BangValue expa, d')
 
-        -- all right propositions focused on are synchronous; this pattern matching is extensive
+        -- all right propositions focused on are synchronous; this pattern matching should be extensive
 
         -- nota: estou a ter alguma dificuldade em visualizar que tipo de proposições vamos ter no lado direito quando focusing na esquerda...
         -- tentei inventar um tipo que me desse focusing com decideLeft mas não estou a conseguir, preciso de ajuda :)
@@ -163,41 +169,62 @@ focus c goal =
         focus' (Just (n, Fun a b)) c@(g, d) goal = do
             vari <- lift get
             let nname = getName vari
+            lift $ put $ vari + 1
             (expb, d') <- focus' (Just (nname, b)) c goal
             let ((expa, d''), vari') = runState (synth (g, d', []) a) (vari+1)
-            lift $ put $ vari'
+            lift $ put vari'
             return (LetIn nname (App (Var n) expa) expb, d'')
             
         ---- &L
-        focus' (Just (n, With a b)) c goal = do
-            (lf, d') <- focus' (Just (n, a)) c goal
-            return (Fst lf, d')
+        focus' (Just (n, With a b)) c goal = do -- como factorizar este código ? 
+            vari <- lift get
+            let nname = getName vari
+            lift $ put $ vari + 1
+            (lf, d') <- trace "try first" $ focus' (Just (nname, a)) c goal
+            trace ("returned first + " ++ show lf ++ " " ++ show d') $ return (LetIn nname (Fst (Var n)) lf, d') -- ainda me faz um bocadinho confusão pensar nas regras assim, parece mesmo que estamos a complicar mesmo não estando, podemos rever a motivação?
             <|> do
-            (rt, d') <- focus' (Just (n, b)) c goal
-            return (Snd rt, d')
+            vari <- lift get
+            let nname = getName vari
+            lift $ put $ vari + 1
+            (rt, d') <- trace "try snd" $ focus' (Just (nname, b)) c goal
+            return (LetIn nname (Snd (Var n)) rt, d')
 
 
         ---- Proposition no longer synchronous --------
 
-        ---- hyp -- left focus is atomic
-        focus' (Just (n, Bool)) (g, d) goal = do -- como fazer isto mais extensível ? onde tenho bool devia ser uma lista de props atómicas certo?
-            guard (Bool == goal)
-            return (Var n, d)
+        ---- left focus is either atomic or not.
+        ---- if it is atomic, it'll either be the goal and instanciate it, or fail
+        ---- if it's not atomic, and it's not left synchronous, unfocus
+        focus' (Just nh@(n, h)) (g, d) goal =
+            if isAtomic h
+               then do
+                   -- left focus is atomic
+                   guard (h == goal) -- if is atomic and not the goal, fail
+                   return (Var n, d)
+               else do
+                   ---- left focus is not atomic and not left synchronous, unfocus
+                   vari <- lift get
+                   let ((e,d'), vari') = runState (synth (g, d, [nh]) goal) vari
+                   lift $ put vari'
+                   return (e, d')
 
         ---- left focus is not atomic and not left synchronous, unfocus
-        focus' (Just a) (g, d) goal = do
-            vari <- lift get
-            let ((e,d'), vari') = runState (synth (g, d, [a]) goal) vari
-            lift $ put vari'
-            return (e, d')
+        -- focus' (Just a) (g, d) goal = do
+        --     vari <- lift get
+        --     let ((e,d'), vari') = runState (synth (g, d, [a]) goal) vari
+        --     lift $ put vari'
+        --     return (e, d')
         
-        ---- right focus is not atomic and not synchronous, unfocus
+        ---- right focus is not atomic and not synchronous, unfocus. if it is atomic we fail
         -- ?? estou a ter tmb alguma difficuldade a visualizar esta situação :)
         focus' Nothing (g, d) goal = do
-            vari <- lift get
-            let ((e,d'), vari') = runState (synth (g, d, []) goal) vari
-            lift $ put vari'
-            return (e, d')
+            if isAtomic goal
+               then empty
+               else do
+                   vari <- lift get
+                   let ((e,d'), vari') = runState (synth (g, d, []) goal) vari
+                   lift $ put vari'
+                   return (e, d')
 
 
 
