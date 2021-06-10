6e50d7162a0641367ddc9fb74d38aa87e341cdd7
romes <rodrigo.m.mesquita@gmail.com>
2021-06-10 22:15:32 +0100

    cleanup util

--- STLLC/app/Program.hs
+++ STLLC/app/Program.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
-module Program where
+module Program (Program(..), CoreProgram(..), ADTD(..), copyMarksTypesModule) where
 
 import qualified Data.Map as Map
 
@@ -50,38 +50,35 @@ instance Show ADTD where
 -- Functions
 -------------------------------------------------------------------------------
 
--- type MarksTypes = Map.Map Int ([(String, Scheme)], Maybe Type)
+type MarksTypes = Map.Map Int ([(String, Scheme)], Maybe Type)
 
--- -- copy marks types to the non-desugared expression from the desugared+inferred expression
--- copyMarksTypes :: Binding -> CoreBinding -> Binding
--- copyMarksTypes (Binding n e) (CoreBinding _ ce) = Binding n $ copyMarksTypes' (getMarksTypes Map.empty ce) e
---     where
---         getMarksTypes :: MarksTypes -> CoreExpr -> MarksTypes
---         getMarksTypes m (CoreSyntax.Mark i c t) = Map.insert i (c,t) m
---         getMarksTypes m (CoreSyntax.Abs _ e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.App e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.TensorValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.LetTensor e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.LetUnit e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.WithValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.Fst e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.Snd e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.InjL _ e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.InjR _ e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.CaseOfPlus e e' e'') = getMarksTypes m e `Map.union` getMarksTypes m e' `Map.union` getMarksTypes m e''
---         getMarksTypes m (CoreSyntax.BangValue e) = getMarksTypes m e
---         getMarksTypes m (CoreSyntax.LetIn e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
---         getMarksTypes m (CoreSyntax.IfThenElse e e' e'') = getMarksTypes m e `Map.union` getMarksTypes m e' `Map.union` getMarksTypes m e''
---         getMarksTypes m a = m
+copyMarksTypesModule :: Program -> CoreProgram -> Program
+copyMarksTypesModule (Program adts bs) (CoreProgram _ cbs) = Program adts $ zipWith copyMarksTypes bs cbs
+    where
+        -- copy marks types to the non-desugared expression from the desugared+inferred expression
+        copyMarksTypes :: Binding -> CoreBinding -> Binding
+        copyMarksTypes (Binding n e) (CoreBinding _ ce) = Binding n $ copyMarksTypes' (getMarksTypes Map.empty ce) e
+            where
+                getMarksTypes :: MarksTypes -> CoreExpr -> MarksTypes
+                getMarksTypes m (CoreSyntax.Mark i c t) = Map.insert i (c,t) m
+                getMarksTypes m (CoreSyntax.Abs _ e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.App e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m (CoreSyntax.TensorValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m (CoreSyntax.LetTensor e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m (CoreSyntax.LetUnit e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m (CoreSyntax.WithValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m (CoreSyntax.Fst e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.Snd e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.InjL _ e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.InjR _ e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.CaseOfPlus e e' e'') = getMarksTypes m e `Map.union` getMarksTypes m e' `Map.union` getMarksTypes m e''
+                getMarksTypes m (CoreSyntax.BangValue e) = getMarksTypes m e
+                getMarksTypes m (CoreSyntax.LetIn e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
+                getMarksTypes m a = m
 
---         copyMarksTypes' :: MarksTypes -> Expr -> Expr
---         copyMarksTypes' m e =
---             editexp
---                 (\case {Syntax.Mark {} -> True; _ -> False})
---                     (\(Syntax.Mark i _ t) ->
---                         let (c, t) = Map.findWithDefault (error "[Copy Marks Types] Failed to find mark index in map, make sure to use the same expression desugared and non-desugared when copying marks types.") i m in
---                             Syntax.Mark i c t) e
-
--- copyMarksTypesModule :: Program -> CoreProgram -> Program
--- copyMarksTypesModule (Program adts bs) (CoreProgram _ cbs) = Program adts $ zipWith copyMarksTypes bs cbs
+                copyMarksTypes' :: MarksTypes -> Expr -> Expr
+                copyMarksTypes' m e =
+                    Syntax.transform (\case
+                        (Syntax.Mark i _ t) -> let (c, t) = Map.findWithDefault (error "[Copy Marks Types] Failed to find mark index in map, make sure to use the same expression desugared and non-desugared when copying marks types.") i m in Syntax.Mark i c t;
+                         x -> x) e
 
--- STLLC/app/Util.hs
+++ STLLC/app/Util.hs
@@ -3,14 +3,11 @@ module Util where
 
 import qualified Data.Map as Map
 
-import CoreSyntax
-import Syntax
-import Program
 
 
-
--- TODO: GIANT REFACTOR
-
+-------------------------------------------------------------------------------
+-- Datatypes
+-------------------------------------------------------------------------------
 
 findDelete :: (Eq a) => a -> [(a, b)] -> [(a, b)] -> (Maybe b, [(a, b)])
 findDelete _ [] _ = (Nothing, [])
@@ -18,34 +15,3 @@ findDelete x ((y,t):xs) acc =
     if x==y then (Just t, reverse acc ++ xs)
     else findDelete x xs ((y,t):acc)
 
-type MarksTypes = Map.Map Int ([(String, Scheme)], Maybe Type)
-
--- copy marks types to the non-desugared expression from the desugared+inferred expression
-copyMarksTypes :: Binding -> CoreBinding -> Binding
-copyMarksTypes (Binding n e) (CoreBinding _ ce) = Binding n $ copyMarksTypes' (getMarksTypes Map.empty ce) e
-    where
-        getMarksTypes :: MarksTypes -> CoreExpr -> MarksTypes
-        getMarksTypes m (CoreSyntax.Mark i c t) = Map.insert i (c,t) m
-        getMarksTypes m (CoreSyntax.Abs _ e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.App e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m (CoreSyntax.TensorValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m (CoreSyntax.LetTensor e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m (CoreSyntax.LetUnit e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m (CoreSyntax.WithValue e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m (CoreSyntax.Fst e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.Snd e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.InjL _ e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.InjR _ e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.CaseOfPlus e e' e'') = getMarksTypes m e `Map.union` getMarksTypes m e' `Map.union` getMarksTypes m e''
-        getMarksTypes m (CoreSyntax.BangValue e) = getMarksTypes m e
-        getMarksTypes m (CoreSyntax.LetIn e e') = getMarksTypes m e `Map.union` getMarksTypes m e'
-        getMarksTypes m a = m
-
-        copyMarksTypes' :: MarksTypes -> Expr -> Expr
-        copyMarksTypes' m e =
-            Syntax.transform (\case
-                (Syntax.Mark i _ t) -> let (c, t) = Map.findWithDefault (error "[Copy Marks Types] Failed to find mark index in map, make sure to use the same expression desugared and non-desugared when copying marks types.") i m in Syntax.Mark i c t;
-                 x -> x) e
-
-copyMarksTypesModule :: Program -> CoreProgram -> Program
-copyMarksTypesModule (Program adts bs) (CoreProgram _ cbs) = Program adts $ zipWith copyMarksTypes bs cbs
