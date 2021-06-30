3042531724fc4290dccba17784ceb3873811f04d
romes <rodrigo.m.mesquita@gmail.com>
2021-06-30 12:00:26 +0100

    fix (w/ last commit) dumb rec function (see t2)

--- STLLC/t2.hs
+++ STLLC/t2.hs
@@ -3,8 +3,4 @@ data Con = Con Nat;
 sum :: (Nat -o (Nat -o Nat));
 sum a b = sum b a;
 
-val :: (Con -o (Con -o Nat));
-val a b = case b of 
-      Con c -> 
-        case a of 
-              Con d -> val (Con c) (Con d);
+synth val :: (Con -o (Con -o Nat));
