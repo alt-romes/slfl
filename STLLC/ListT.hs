1e1a1688830233cc5fa62f5916f73f173f558307
romes <rodrigo.m.mesquita@gmail.com>
2021-07-14 16:40:28 +0100

    fixes according to prof

---
+++ STLLC/ListT.hs
@@ -0,0 +1,14 @@
+data List a = Nil | Cons (a * List a);
+
+data Tree a = Tip | Node (a * List (Tree a));
+
+
+
+#synth test :: ((A -o B) -> Tree A -o Tree B);
+
+synth concat :: (List (List Int) -o List Int);
+
+synth concat2 :: (List (List A) -o List A);
+
+synth zip :: ((A -o B -o C) -> List A -o List B -o List C);
+
